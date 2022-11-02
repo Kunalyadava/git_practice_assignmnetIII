ea9792b7c26fe6f4ea6c0402694d8fed0746add6
kunalyadava <kunalkr1857@gmail.com>
2022-11-02 13:35:15 +0530

    added prime and palindrome code

---
+++ prime.js
@@ -0,0 +1,20 @@
+function check_prime(n)
+{
+​
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+ 
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+​
+console.log(check_prime(37));
